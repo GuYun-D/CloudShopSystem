c23fbea51562e81fe442c45541bfbe7c7e9246d9
GuYun-D <1946306647@qq.com>
2021-07-15 16:42:26 +0800

    添加与修改

--- src/views/product/attr/List.vue
+++ src/views/product/attr/List.vue
@@ -7,10 +7,17 @@
       <!-- 列表页数据 -->
       <!-- 
         此处不能用v-if
-        
+        不然就会把数据干掉了
        -->
       <div v-show="isShowList">
-        <el-button disabled type="primary" icon="el-icon-plus"
+        <!-- 
+          根据3级分类的存在与否，动态显示按钮的可操作性
+         -->
+        <el-button
+          type="primary"
+          :disabled="category3Id === '' ? true : false"
+          icon="el-icon-plus"
+          @click="showAddDiv"
           >添加属性</el-button
         >
         <el-table border style="margin-top: 20px" :data="attrList">
@@ -38,7 +45,7 @@
                 content="修改"
                 placement="top-end"
               >
-                <el-button type="warning" icon="el-icon-edit"></el-button>
+                <el-button type="warning" icon="el-icon-edit" @click="showUpdataDiv(row)"></el-button>
               </el-tooltip>
 
               <el-tooltip
@@ -65,28 +72,57 @@
           </el-form-item>
         </el-form>
 
-        <el-button type="primary" icon="el-icon-plus">添加属性值</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          :disabled="attrForm.attrName === '' ? true : false"
+          @click="addAttrValue"
+          >添加属性值</el-button
+        >
         <el-button type="info">取消</el-button>
 
-        <el-table width="100%" border style="margin: 20px 0">
+        <el-table
+          :data="attrForm.attrList"
+          width="100%"
+          border
+          style="margin: 20px 0"
+        >
           <el-table-column
             width="80"
             type="index"
             label="序号"
             align="center"
           ></el-table-column>
-          <el-table-column label="属性值名称" prop=""></el-table-column>
-          <el-table-column label="操作" prop=""></el-table-column>
+          <el-table-column label="属性值名称">
+            <template slot-scope="{ row, $index }">
+              <el-input
+                v-model="row.valueName"
+                placeholder="请输入属性值名称"
+                size="normal"
+                clearable
+              ></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" prop="">
+            <template>
+              <el-tooltip content="删除" placement="top" effect="dark">
+                <el-button type="danger" size="default" icon="el-icon-delete"></el-button>
+                
+              </el-tooltip>
+              
+            </template>
+          </el-table-column>
         </el-table>
 
         <el-button type="primary">保存</el-button>
-        <el-button>取消</el-button>
+        <el-button @click="isShowList = true">取消</el-button>
       </div>
     </el-card>
   </div>
 </template>
 
 <script>
+import cloneDeep from 'lodash/cloneDeep'
 export default {
   name: "attr",
   data() {
@@ -96,7 +132,7 @@ export default {
       category3Id: "",
       attrList: [],
       // 控制列表页和修改页的显示与隐藏
-      isShowList: false,
+      isShowList: true,
       /**
        * 添加属性要收集的表单样子
        * {
@@ -113,10 +149,56 @@ export default {
        *    "id": 0
        * }
        */
-      attrForm: { attrName: "" },
+      attrForm: {
+        attrName: "",
+        attrList: [],
+        categoryId: 0,
+        categoryLevel: 3,
+      },
     };
   },
   methods: {
+    // 修改品牌
+    showUpdataDiv(row){
+      this.isShowList = false
+      // 结构出数据，防止同时变
+      // 深拷贝
+      this.attrForm = cloneDeep(row)
+    },
+
+    // 点击添加按钮
+    showAddDiv() {
+      this.isShowList = false;
+      /**
+       * 清空收集的对象以及清空添加表单
+       * {
+       *  categoryId: 0,
+       *  categoryLevel: 3
+       * }
+       * categoryLevel是固定的，因为只有选择了三级分类id，才会发送请求
+       * categoryId 而是要在用户点击添加，才收集
+       */
+      this.attrForm = {
+        attrName: "",
+        attrList: [],
+        categoryId: this.category3Id,
+        categoryLevel: 3
+      }
+    },
+
+    /**
+     * 点击添加属性值按钮
+     * 给属性值列表当中添加一个空的对象，保证表格当中首先出现一行
+     */
+    addAttrValue() {
+      this.attrForm.attrList.push({
+        // 该id是属性值所属属性的id，如果是添加属性，这个id是没有的，如果是修改属性，该id是有的
+        attrId: this.attrForm.id,
+        // 这里只是添加了一个空对象，属性值都还没有，只是在占位，让用户自己输入
+        valueName: "",
+      });
+    },
+
     changeCategory({ categoryId, level }) {
       /**
        * 将分类id存储起来，以便发送请求
